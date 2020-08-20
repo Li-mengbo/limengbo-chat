cc09a82df6c7ef0681672ca3cdd9886443e4ff34
limengbo <15600099705@163.com>
2020-08-20 19:18:04 +0800

    增加移动端适配，待测试

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    'postcss-px-to-viewport': {
+      viewportWidth: 375,
+      viewportHeight: 667,
+      unitPrecision: 3,
+      viewportUnit: 'vw',
+      selectorBlackList: [],
+      minPixelValue: 1,
+      mediaQuery: false,
+      exclude: /(\/|\\)(node_modules)(\/|\\)/,
+    },
+  },
+};
